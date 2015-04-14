25252d939be55717dc237de75e7d2322efe2126b
Zhenyu Han <zh0001@bravemail.uncp.edu>
2015-04-14 11:02:33 -0400

    modified
    made some change to show an example

--- api/yelp/attractions_id.py
+++ api/yelp/attractions_id.py
@@ -7,6 +7,7 @@ This is a temporary script file.
 
 import json
 import urllib
+import pprint
 
 def get_city_attractions(topic_id):
     service_url = 'https://www.googleapis.com/freebase/v1/topic'
@@ -18,4 +19,6 @@ def get_city_attractions(topic_id):
     return topic
     # Eden, please note that if you need the attractions, call: get_city_attractions(topic_id)['property']['/location/location/contains']
     # geo info, get_city_attractions(topic_id)['/location/location/geolocation']
-
+if __name__ == '__main__':
+    city_id = '/m/04jpl' # topic id of London for example
+    pprint.pprint(get_city_attractions(city_id)['property']['/location/location/contains'])
